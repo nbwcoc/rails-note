2e4856767a60d04f2ccdeabc8a06262266dad1c4
John Smith <nbwcoc@openmailbox.org>
2018-08-13 18:01:18 -0400

    delete

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -31,6 +31,13 @@ class NotesController < ApplicationController
     end
   end
 
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+
+    redirect_to notes_path
+  end
+
   private
   def note_params
     params.require(:note).permit(:title, :text)
